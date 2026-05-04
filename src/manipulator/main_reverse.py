077a3bc68bbf4dc51f173498603912eed5068dab
Ilya Koloshin <ilya.koloshin@gmail.com>
2026-05-04 17:12:26 +0400

    refactor main_reverse to use two link manipulator class

--- src/manipulator/main_reverse.py
+++ src/manipulator/main_reverse.py
@@ -1,12 +1,10 @@
-import math
-
 from manipulator.config import LINK_1_LENGTH, LINK_2_LENGTH, DEFAULT_ANGLE_1, DEFAULT_ANGLE_2
 from manipulator.math_utils import distance
 from manipulator.motion import generate_angle_path, generate_position_path
+from manipulator.robot import TwoLinkManipulator
 from manipulator.visualizer import animate_motion_path
-from manipulator.forward_kinematics import first_link_end, second_link_end
 from manipulator.printer import print_manipulator_state
-from manipulator.inverse_kinematics import find_angles
+
 
 def main():
 
@@ -22,34 +20,27 @@ def main():
         print("Invalid elbow mode. Use 'up' or 'down'.")
         return
 
-    angles = find_angles(x, y, LINK_1_LENGTH, LINK_2_LENGTH, elbow=elbow)
+    target_point = (x, y)
 
-    if angles is None:
+    arm = TwoLinkManipulator(LINK_1_LENGTH, LINK_2_LENGTH)
+
+    if not arm.move_to(target_point, elbow): # find th current angles
+
         print("Target is unreachable")
         return
 
-    a, b = angles
-
-    angle_1_link = math.degrees(a)
-    angle_2_link = math.degrees(b)
-    first_point = first_link_end(LINK_1_LENGTH, angle_1_link)
-    second_point = second_link_end(
-        LINK_1_LENGTH,
-        LINK_2_LENGTH,
-        angle_1_link,
-        angle_2_link
-    )
+    first_point,second_point = arm.get_position()
 
     print_manipulator_state(
         LINK_1_LENGTH,
         LINK_2_LENGTH,
-        angle_1_link,
-        angle_2_link,
+        arm.current_angle_1_deg,
+        arm.current_angle_2_deg,
         first_point,
         second_point
     )
 
-    target_point = (x, y)
+
 
     print(f"Target point: {target_point}")
     print(f"End effector: {second_point}")
@@ -67,8 +58,8 @@ def main():
     angle_path = generate_angle_path(
         start_angle_1_deg=start_angle_1_deg,
         start_angle_2_deg=start_angle_2_deg,
-        target_angle_1_deg=angle_1_link,
-        target_angle_2_deg=angle_2_link,
+        target_angle_1_deg=arm.current_angle_1_deg,
+        target_angle_2_deg=arm.current_angle_2_deg,
         steps=motion_steps
     )
 
